68af1f34810b11f1aa5641ff0eda873877b13fb4
Kundan-Arekar <kundan_arekar@epam.com>
2024-02-14 21:30:56 +0530

    commit

---
+++ src/main/java/RegressionTest/Regression.java
@@ -0,0 +1,10 @@
+package RegressionTest;
+
+public class Regression {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Regression!"); 
+
+	}
+
+}
---
+++ src/main/java/SmokeTest/Smoke.java
@@ -0,0 +1,10 @@
+package SmokeTest;
+
+public class Smoke {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello, Smoke!"); 
+	}
+
+}
